d8455545975d3e349a964956f8636ee3ebf842ac
z2283975 <z2283975@163.com>
2024-12-14 00:46:46 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -16,3 +16,7 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 5-添加 PassWall 插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# 6-添加  Themes
+git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
